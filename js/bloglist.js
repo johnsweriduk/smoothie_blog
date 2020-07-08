51186537b43f09de6fd759866c08d6c4e732e668
Jessica Teekasingh <jessi@Jessicas-MBP.attlocal.net>
2020-07-08 12:27:42 -0400

    bloglist.js complete

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,12 +1,66 @@
-class App extends React.Component {
+class BlogPost extends React.Component {
   state = {
     posts: []
   }
 
+  componentDidMount = () => {
+        axios.get('/blogs').then(
+            response => {
+                this.setState({
+                    posts: response.data
+                });
+            }
+        )
+    }
+
 
   render = () => {
     return <div>
+      <h2>Created Posts</h2>
+        <ul>
+        {
+          this.state.posts.map(
+            (blog) => {
+              return <li key={blog.id}>
+              <div className="Posted">
 
+                <div className={"image"}>
+                <img src={blog.image} />
+                </div>
+                  <div className={"overlay"}>
+
+                  <div className={"title"}>
+
+                  <h3>{blog.title}</h3>
+                  </div>
+
+                  <div className={"snippet"}>
+                  <p>{blog.snippet}</p>
+                  </div>
+
+                  <div className={"blog-meta"}>
+
+                  <div className={"author"}>
+
+                   <p>{blog.author}</p>
+                   </div>
+
+                   <div className={"published"}>
+                   <p>{blog.created_at}</p>
+                   </div>
+
+                   <div className={"likes"}>
+                   <p>{blog.likes}</p>
+
+                   </div>
+                 </div>
+                </div>
+              </div>
+              </li>
+            }
+          )
+        }
+        </ul>
     </div>
   }
 }
